1c57a0aeb3e920fef4872591a372784e80029884
VegaAnderson <136628675+VegaAnderson@users.noreply.github.com>
2023-06-15 14:59:38 +0000

    ngmfj

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Tallerandersonvega
-Taller de entorno de desarrollo
+# Taller Nro. 1 Entorno de desarrollo
+
+- Nombre: Anderson Vega
+
+
+## Descripción
+
+## Funcionamiento
+
+![](img/south-tyrol-8023224_1280.jpg)
---
+++ programa.py
@@ -0,0 +1,10 @@
+from modulo_funciones import sumar,restar,multiplicar,dividir
+
+print(sumar(2,3))
+print(restar(4,2))
+print(multiplicar(3,3))
+print(dividir(10,2))
+
+from algebra import distancia 
+
+print(distancia(2, 4, 3,6))
